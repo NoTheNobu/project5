35efcd5497407f3b6879f3b98cfff8e65ec1918d
David Cisneros <dalcoce4@stu.southtexascollege.edu>
2025-03-03 23:38:38 -0600

    another commit

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { faker } from '@faker-js/faker'
+    const firstname = faker.food.adjective()
+    const lastname = faker.food.dish()
+    const jobtitle = faker.food.ingredient()
+    const bio = faker.food.description(2)
+    const username = faker.internet.userName({firstName: firstname, lastName: lastname}).toLowerCase()
+</script>
+
+<template>
+    <RouterLink :to="{name: 'CardDetail', params: {id: username}}">
+        <div class="border border-purple-900 rounded-lg shadow bg-gray-300 h-full">
+            <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'Food', height: 480, width: 960})" />
+            <div class="p-4">
+                <p class="mb-2 text-2x1 font-bold">{{ firstname }} {{ lastname }}</p>
+                <p class="mb-3 font-normal text-red-600">{{ jobtitle }}</p>
+                <p class="mb-3 font-normal text-blue-600">{{ bio }}</p>
+            </div>
+        </div>
+    </RouterLink>
+</template>
---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAB7C6FPWXuwpPp3Wm3RI40HzxAmILpq7w",
+  authDomain: "project5-aa060.firebaseapp.com",
+  projectId: "project5-aa060",
+  storageBucket: "project5-aa060.firebasestorage.app",
+  messagingSenderId: "961319259035",
+  appId: "1:961319259035:web:88a5a56d6f6f85cfabef3e"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
